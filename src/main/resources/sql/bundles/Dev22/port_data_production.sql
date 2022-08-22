9ab63e432d6929a218d6adea64f0309275d734c0
santhosh <santhoshkumar1233@gmail.com>
2022-08-22 08:41:47 +0530

    sample

--- src/main/resources/sql/bundles/Dev22/port_data_production.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO showletcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('Dev22', '<?xml version="1.0" encoding="UTF-8"?>
-<properties>
-<property key="en">Test</property>
-<property key="it">Test</property>
-</properties>', NULL, NULL, NULL, NULL, 1, NULL);
